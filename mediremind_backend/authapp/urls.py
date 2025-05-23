24df2b678370ce3bebf665240b598744bb20f585
Bouric Enos <bouricenos0@gmail.com>
2025-05-23 14:12:38 +0300

    Created  patients app and Staff profiles app and implemented dashboard views and tested endpoints are working as expected.

--- mediremind_backend/authapp/urls.py
+++ mediremind_backend/authapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("register/", views.register_user),
     path("login/", views.login_user),
     path("forgot-password/", views.forgot_password),
+    path("logout/", views.logout_user),
 ]
